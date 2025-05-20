8221a6b31cc94a6a2d7b805dd2ca61cfd233b7cb
GabrielPKTN <gabriellacerda20059@outlook.com>
2025-05-20 18:04:18 -0300

    Iniciando projeto de um sistema de coleção de jogos com spring

---
+++ src/test/java/br/com/dev/lacerda/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.lacerda.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
