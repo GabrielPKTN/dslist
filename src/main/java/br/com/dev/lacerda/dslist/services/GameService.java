a57a3d0bd43a1957ec7a39d957ae134d1e845392
GabrielPKTN <gabriellacerda20059@outlook.com>
2025-05-21 17:17:58 -0300

    GET game pelo id

--- src/main/java/br/com/dev/lacerda/dslist/services/GameService.java
+++ src/main/java/br/com/dev/lacerda/dslist/services/GameService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.dev.lacerda.dslist.dto.GameDTO;
 import br.com.dev.lacerda.dslist.dto.GameMinDTO;
 import br.com.dev.lacerda.dslist.entities.Game;
 import br.com.dev.lacerda.dslist.repositories.GameRepository;
@@ -16,7 +18,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	@GetMapping
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
